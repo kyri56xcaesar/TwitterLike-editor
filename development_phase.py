c0b97d71b5a42d6120dc5dfbcabceb858ffe27a3
Kyriakos Chalvatzis <kyriakos.halvatzis@gmail.com>
2022-11-09 20:31:00 +0200

    task handler

--- development_phase.py
+++ development_phase.py
@@ -5,13 +5,8 @@ import os
 import ast
 
 
-
-# TODO : 
-#   -handle tasks and save
-#   -finish update
-#   -scoope delete
-
-
+# Name of the file used
+file_name = "test.json"
 
 # list of commands available
 s_commands = ['c', 'r', 'u', 'd', '$', '-', '+', '=', 'q', 'w', 'h']
@@ -39,7 +34,7 @@ tasks = []
 # Initialize the t_ID list with the index/number of each line
 # Consider starting from 1. As the first tweet is ID is 1.
 def configureID():
-     with open("tweetdhead300000.json", "rb") as file:
+     with open(file_name, "rb") as file:
 
         for i, line in enumerate(file):
             t_ID.append(i)
@@ -63,36 +58,40 @@ def help():
 def print_table(f, to):
 
     for i in range(to - f):
-        print(t_ID[f + i])
+        if f + i < len(t_ID):
+            print(t_ID[f + i])
 
 # Create a tweet function handler.      --> DONE
 def create_tweet():
+
+    global current_tweet
+    global current_tweet_id
+
     print("Creating a tweet...")
 
     # text
     ttext = input("Enter text: ")
 
     # created at
-    tdate = date.today().strftime('%a %b %d %H:%M:%S +0200%Z %Y')   # Day Month Day/Month HH:mm:ss timezone year
- 
+    tdate = datetime.today().strftime('%a %b %d %H:%M:%S +0200%Z %Y')   # Day Month Day/Month HH:mm:ss timezone year
+    print(tdate)
+
     # Create a new JSON tweet
     new_tweet = json.dumps({'text': ttext,'created at':tdate})
     
     # Schedule a task
     tasks.append(({"n":new_tweet}, current_tweet_id))
 
-    ## TO BE MOVED
-    # save to file?
-    with open("test.json", "a") as file:
-        file.write(new_tweet+"\n")
-
     # Set this new tweet as the current tweet 
-    current_tweet_id = t_ID[-1]
+    if t_ID != []:
+        current_tweet_id = int(t_ID[-1]) + 1
+    else:
+        current_tweet_id = 0
     # Set the new current_tweet
     current_tweet = new_tweet
     
     # Update the tweetID list
-    t_ID.append(current_tweet)
+    t_ID.append(current_tweet_id)
     
 
 
@@ -124,12 +123,12 @@ def read_tweet(number, prompt=False):
     current_tweet_id = number - 1
 
 
-    number = t_ID[current_tweet_id]
+    #number = t_ID[current_tweet_id]
 
     # Find the corresponding tweet
-    with open("tweetdhead300000.json", "r") as rfile:
+    with open(file_name, "r") as rfile:
         for i, line in enumerate(rfile):
-            if i == number:
+            if i == current_tweet_id:
                 current_tweet=line
                 break
 
@@ -148,19 +147,20 @@ def update_tweet(number, prompt=False):
     # Get the new text input
     new_text = input("Enter text: ")
 
-    tdate = date.today().strftime('%a %b %d %H:%M:%S +0200%Z %Y')   # Day Month Day/Month HH:mm:ss timezone year
+    tdate = datetime.today().strftime('%a %b %d %H:%M:%S +0200%Z %Y')   # Day Month Day/Month HH:mm:ss timezone year
     
     # Update the tweet
-    # break the string into parts
+    # create a dictionary out of the tweet
     current_tweet = json.loads(current_tweet)
     current_tweet.update({"text":new_text})
     current_tweet.update({"created_at":tdate})
-
+    current_tweet = '{"'+str(list(current_tweet.keys())[0])+'": "'+current_tweet.get("text")+'", "'+str(list(current_tweet.keys())[1])+'": "'+current_tweet.get("created_at")+'"}\n'
    
+
+    # for improvement.
     # save in memory
-    mem_tweets.append(current_tweet)
-
-
+    #mem_tweets.append({current_tweet_id:current_tweet})
+    
 
     # Schedule task
     tasks.append(({"u":current_tweet}, current_tweet_id))
@@ -180,10 +180,11 @@ def delete_tweet():
 
     print("Deleting a tweet...")
 
+
+    # Tasking IMPROVEMENT
     tasks.append(({"d":current_tweet}, current_tweet_id))
 
     ## UPDATE TWEET TABLE
-
     t_ID.remove(t_ID[current_tweet_id])
     
   
@@ -210,7 +211,7 @@ def read_Ltweet():
 
     
     
-    with open("tweetdhead300000.json", "rb") as file:
+    with open(file_name, "rb") as file:
 
         try:
             file.seek(-2, os.SEEK_END)
@@ -285,7 +286,7 @@ def quit(toSave=False):
     if toSave:
         save_prompt = input("\n*WARNING*\nContents will not be saved. Would you like to save them [Y]? ")
         if save_prompt.upper() == "Y":
-            save();
+            save()
     exit()
     
 ## SAVE method - Overwrites the file --> TODO
@@ -295,10 +296,59 @@ def save():
 
     print("\n\nContents saved!")
     
-    
+# task structure : ({whatToDo, tweet}, tId)
+def task_handler():
 
-    
 
+    while tasks != []:
+        task = tasks.pop()
+        if list(task[0].keys())[0] == "n":
+            with open(file_name, "a") as file:
+                try:
+                    print(file.tell())
+                    file.seek(0, 2)
+                    print(file.tell())
+                    if file.tell() != 0:
+                        file.write("\n")
+                    print(task[0].get("n"))
+                    file.write(task[0].get("n"))
+                    #json.dump(task[0].get("n"), file, indent=0)
+                except OSError:
+                    print("error")
+                    file.seek(0)
+        
+     
+     
+        if list(task[0].keys())[0] == "d":
+            lines = []
+            # delete the tweet from the file
+            with open(file_name, "r") as fp:
+                lines = fp.readlines()
+
+            with open(file_name, "w") as fp:
+                for number, line in enumerate(lines):
+                    if number != task[1]:
+                        fp.write(line)
+     
+            
+        if list(task[0].keys())[0] == "u":
+
+            lines = []
+            # read all the lines
+            with open(file_name, "r") as fp:
+                lines = fp.readlines()
+
+            # rewrite all the lines but with the updated ones
+            with open(file_name, "w") as fp:
+                for number, line in enumerate(lines):
+                    if number == current_tweet_id:
+                        fp.write(current_tweet)
+                    else:
+                        fp.write(line)
+
+                
+        
+        
 
 
 if __name__ == "__main__":
@@ -387,6 +437,11 @@ if __name__ == "__main__":
             elif command == 'ph':
                 print_table(int(input("From: ")), int(input("To: ")))
 
+            elif command == 'id':
+                print("Current ID is: " + str(current_tweet_id)+"\nCorresponding to: " + str(t_ID[current_tweet_id]))
+
             # if false input or no input. print help
             #elif args==[]:
             #    help()
+
+            task_handler()
